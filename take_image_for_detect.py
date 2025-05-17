e5a1a891b7b278ea4b8d8609a06db920343e7272
haphm <haphm@github.com>
2025-05-17 16:30:22 +0300

    add color detection function

--- take_image_for_detect.py
+++ take_image_for_detect.py
@@ -29,6 +29,23 @@ def _detect_objects(image: np.ndarray) -> None:
                 f.write(str(xywh[i]) + "\n")
             result.save("test/result.png")  # save the image with predictions
 
+def _detect_color(frame, x_coor, y_coor) -> str:
+    color_thresholds = [10, 20, 30, 85, 130, 160, 179]
+    color_name = ['red', 'brown', 'yellow', 'green', 'blue', 'violet', 'red']
+    hsv_pixel = frame[x_coor, y_coor]
+    h, s, v = hsv_pixel
+    print(f"h: {h}, s: {s}, v: {v}")
+    if v < 50:
+        return 'black'
+    if s < 30 and v > 200:
+        return 'white'
+    for i, thresh in enumerate(color_thresholds):
+        if h <= thresh:
+            color = color_name[i]
+            return color
+    return "Undefined"
+
+
 def _main() -> None:
     # Specify the directory name
     test_directory = "test"
@@ -57,7 +74,7 @@ def _main() -> None:
 
     print("Image captured.")
 
-    with app:
+    with (app):
         data_file = "test/detect.zdf"
         print(f"Reading ZDF frame from file: {data_file}")
         frame = zivid.Frame(data_file)
@@ -65,6 +82,7 @@ def _main() -> None:
 
         print("Converting to BGR image in OpenCV format")
         bgr = _point_cloud_to_cv_bgr(point_cloud)
+        hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
 
         bgr_image_file = "test/ImageRGB.png"
         print(f"Visualizing and saving BGR image to file: {bgr_image_file}")
@@ -84,6 +102,9 @@ def _main() -> None:
                 xywh = np.array(xywh)
                 x_pixel = int(xywh[0][0])
                 y_pixel = int(xywh[0][1] + 150)
+                color_detected = _detect_color(hsv, y_pixel -150, x_pixel)
+                print(f"Object at x_coord: {x_pixel}, y_coord: {y_pixel -150} is in {color_detected} color")
+
                 if y_pixel > 1200:
                     y_pixel = 1200
                 x_value = z_color_map[y_pixel, x_pixel, 0]
@@ -92,7 +113,7 @@ def _main() -> None:
                 if 'nan' not in str(x_value) and 'nan' not in str(y_value) or 'nan' not in str(z_value):
                     with open("test/xyz_coordinate.txt", "a") as file:
                         print(f"XYZ value at pixel ({x_pixel}, {y_pixel}): {x_value} {y_value} {z_value}")
-                        file.write(f"{x_value} {y_value} {z_value} {1}\n")
+                        file.write(f"{x_value} {y_value} {z_value} {1} {color_detected}\n")
 
                 with open("test/xyz_coordinate.txt", "r") as file:
                     lines = file.readlines()
