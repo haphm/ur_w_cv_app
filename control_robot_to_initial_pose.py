6c118d7841c0502725a6b8e5b445c502d1370bdd
haphm <haphm@github.com>
2025-05-30 11:03:26 +0300

    add read current robot position function

--- control_robot_to_initial_pose.py
+++ control_robot_to_initial_pose.py
@@ -1,4 +1,5 @@
 from rtde_control import RTDEControlInterface
+from rtde_receive import RTDEReceiveInterface
 
 ROBOT_IP = "192.168.50.23"
 ROBOT_PORT = 30004
@@ -19,7 +20,16 @@ def move_to_position(position):
         rtde_control.stopScript()
         print("Disconnected from robot")
 
+def read_current_position():
+    rtde_receive = RTDEReceiveInterface(ROBOT_IP)
+    current_pose = rtde_receive.getActualTCPPose()
+    print(f"Current Pose: {current_pose}")
+    input("Press enter to continue...")
+
 if __name__ == "__main__":
+    # # Read current robot position
+    # read_current_position()
+
     # Example position to move to
-    target_position = [-0.052069181437518125, -0.27607119937006347, 0.4008986149866134, -0.03799443728613493, 2.5927110603628365, -1.7112709281720009]
+    target_position = [-0.05210053790641115, -0.27605043364346304, 0.4070199063449296, -0.039457233465660886, 2.5466319129173374, -1.7801884436371929]
     move_to_position(target_position)
